2009817665b267fade3d3c23511fe0eebee19778
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-10-07 22:34:31 +0300

    task25

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число:");
+int firstNum = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:");
+int secondNum = int.Parse(Console.ReadLine()!);
+int result = 1;
+
+Power(firstNum, secondNum);
+int Power(int a, int b)
+{
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+Console.Write(result);
